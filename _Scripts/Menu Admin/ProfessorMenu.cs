a0e5e6f843d1aa44338729d0dc9f463e00248c0d
Rafael Menon <menonrafael7@gmail.com>
2025-06-27 12:38:38 -0300

    Menu Admin

---
+++ _Scripts/Menu Admin/ProfessorMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class AdminPanelManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ShowAdminMenu();   
+    }
+
+    public void ShowAdminMenu()
+    {
+
+    }
+
+}
