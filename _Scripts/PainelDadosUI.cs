3ab4d2715b7f2a19b83f61017a388ce91d7844a2
unknown <yan2005leal@gmail.com>
2025-06-27 23:59:02 -0300

    Finalizações

--- _Scripts/PainelDadosUI.cs
+++ _Scripts/PainelDadosUI.cs
@@ -1,22 +1,22 @@
 using TMPro;
 using UnityEngine;
 
-// Este script serve apenas para guardar as referências dos campos de texto
+// Este script serve apenas para guardar as referï¿½ncias dos campos de texto
 // do seu prefab de detalhes.
-public class PainelDadosUI : MonoBehaviour
+public class PainelDadosUI2 : MonoBehaviour
 {
-    [Header("Referências dos Textos da Fase 1")]
+    [Header("Referï¿½ncias dos Textos da Fase 1")]
     public TextMeshProUGUI fase1TextoTempo;
     public TextMeshProUGUI fase1TextoPontuacao;
     public TextMeshProUGUI fase1TaxaDeAcerto;
 
-    [Header("Referências dos Textos da Fase 2")]
+    [Header("Referï¿½ncias dos Textos da Fase 2")]
     public TextMeshProUGUI fase2TextoTempo;
     public TextMeshProUGUI fase2TextoAcertos;
     public TextMeshProUGUI fase2TextoErros;
     public TextMeshProUGUI fase2TextoTaxaDeAcerto;
 
-    [Header("Referências dos Textos da Fase 3")]
+    [Header("Referï¿½ncias dos Textos da Fase 3")]
     public TextMeshProUGUI fase3TextoTempo;
     public TextMeshProUGUI fase3TextoAcertos;
     public TextMeshProUGUI fase3TextoErros;
